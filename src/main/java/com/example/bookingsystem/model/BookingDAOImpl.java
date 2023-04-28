695232afdc8ae47f4e9891238f0aa721ba8044e7
PernillePersson <78405936+PernillePersson@users.noreply.github.com>
2023-04-28 11:45:37 +0200

    DAO created

---
+++ src/main/java/com/example/bookingsystem/model/BookingDAOImpl.java
@@ -0,0 +1,15 @@
+package com.example.bookingsystem.model;
+
+import java.util.List;
+
+public class BookingDAOImpl implements BookingDAO {
+    @Override
+    public List<Booking> getAllBooking() {
+        return null;
+    }
+
+    @Override
+    public void addBooking() {
+
+    }
+}
