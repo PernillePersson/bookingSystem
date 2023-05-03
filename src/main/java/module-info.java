781ea68b3fcd46a7e92542f050bbc2f317c8cc3c
PernillePersson <78405936+PernillePersson@users.noreply.github.com>
2023-05-03 14:25:00 +0200

    Changes by Marc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,7 @@ module com.example.bookingsystem {
     requires java.sql;
     requires jakarta.mail;
 
-    exports userBooking.Controllers;
-    opens userBooking.Controllers to javafx.fxml;
-    exports userBooking;
-    opens userBooking to javafx.fxml;
+
     opens com.example.bookingsystem to javafx.fxml;
     exports com.example.bookingsystem;
 }
