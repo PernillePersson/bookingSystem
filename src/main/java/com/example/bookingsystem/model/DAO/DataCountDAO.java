df62e0cbc69c5d9f828537fc13232e2eba5792b5
m4gn3s1um <madmedmig@gmail.com>
2023-05-16 16:53:13 +0200

    Statistics side done

--- src/main/java/com/example/bookingsystem/model/DAO/DataCountDAO.java
+++ src/main/java/com/example/bookingsystem/model/DAO/DataCountDAO.java
@@ -2,14 +2,17 @@ package com.example.bookingsystem.model.DAO;
 
 import com.example.bookingsystem.model.DataCount;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface DataCountDAO {
 
 
-    public List<DataCount> importData(String s1);
+    public List<DataCount> importData(String s1, LocalDate d1, LocalDate d2);
 
 ////
 
     public List<DataCount> importOrgData(String s1);
+
+    public List<DataCount> importOrgStatData(String s1, LocalDate d1, LocalDate d2);
 }
--- src/main/java/com/example/bookingsystem/model/DAO/DataCountDAOImpl.java
+++ src/main/java/com/example/bookingsystem/model/DAO/DataCountDAOImpl.java
@@ -4,10 +4,8 @@ import com.example.bookingsystem.model.ConnectionSingleton;
 import com.example.bookingsystem.model.DAO.DataCountDAO;
 import com.example.bookingsystem.model.DataCount;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,15 +16,16 @@ public class DataCountDAOImpl implements DataCountDAO {
         con = ConnectionSingleton.getInstance().getConnection();
     }
 
-////
     @Override
-    public List<DataCount> importData(String s1) {
+    public List<DataCount> importData(String s1, LocalDate d1, LocalDate d2) {
 
         List<DataCount> data = new ArrayList<>();
         try{
-            PreparedStatement ps = con.prepareStatement("SELECT DISTINCT BookingForløb.boFoID,BookingForløb.bookingID,BookingForløb.forløbID FROM BookingForløb,Forløb,Booking WHERE Forløb.forløbID = BookingForløb.forløbID AND Forløb.forløb = ?;");
+            PreparedStatement ps = con.prepareStatement("SELECT DISTINCT BookingForløb.boFoID,BookingForløb.bookingID,BookingForløb.forløbID FROM BookingForløb,Forløb,Booking WHERE Forløb.forløbID = BookingForløb.forløbID AND Forløb.forløb = ? AND Booking.bookingDate BETWEEN ? AND ?;");
 
             ps.setString(1,s1);
+            ps.setDate(2, Date.valueOf(d1));
+            ps.setDate(3, Date.valueOf(d2));
             ResultSet rs = ps.executeQuery();
 
             DataCount dc;
@@ -52,6 +51,33 @@ public class DataCountDAOImpl implements DataCountDAO {
             PreparedStatement ps = con.prepareStatement("SELECT DISTINCT BookingOrg.boOrgID,BookingOrg.bookingID,BookingOrg.organisationID FROM BookingOrg,Organisation,Booking WHERE BookingOrg.organisationID = Organisation.organisationID AND Organisation.organisation = ?;");
 
             ps.setString(1,s1);
+            ResultSet rs = ps.executeQuery();
+
+            DataCount dc;
+
+            while (rs.next()){
+                int id1 = rs.getInt(1);
+                int id2 = rs.getInt(2);
+                int id3 = rs.getInt(3);
+
+                dc = new DataCount(id1, id2, id3);
+                data.add(dc);
+            }
+        } catch (SQLException e) { System.out.println(e); }
+
+        return data;
+    }
+
+    @Override
+    public List<DataCount> importOrgStatData(String s1, LocalDate d1, LocalDate d2) {
+        List<DataCount> data = new ArrayList<>();
+        try{
+            PreparedStatement ps = con.prepareStatement("SELECT DISTINCT BookingOrg.boOrgID,BookingOrg.bookingID,BookingOrg.organisationID FROM BookingOrg,Organisation,Booking WHERE BookingOrg.organisationID = Organisation.organisationID AND Organisation.organisation = ? AND Booking.bookingDate BETWEEN ? AND ?;");
+
+            ps.setString(1,s1);
+            ps.setDate(2, Date.valueOf(d1));
+            ps.setDate(3, Date.valueOf(d2));
+
             ResultSet rs = ps.executeQuery();
 
             DataCount dc;
