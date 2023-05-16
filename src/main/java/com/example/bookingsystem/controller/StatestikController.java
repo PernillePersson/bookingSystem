e37fc05046fd99abdd39a766e6382d7cf61f23b5
fewture <113719482+m4gn3s1um@users.noreply.github.com>
2023-05-16 21:35:24 +0200

    Added more functionality to piechart

--- src/main/java/com/example/bookingsystem/controller/StatestikController.java
+++ src/main/java/com/example/bookingsystem/controller/StatestikController.java
@@ -5,9 +5,12 @@ import com.example.bookingsystem.model.DAO.DataCountDAO;
 import com.example.bookingsystem.model.DAO.DataCountDAOImpl;
 import com.example.bookingsystem.model.DataCount;
 import javafx.beans.Observable;
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -17,9 +20,14 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -83,6 +91,8 @@ public class StatestikController {
 
         forløbChart.getData().clear();
 
+
+
         List<DataCount> ideData = dc.importData("Idéfabrikken",fChartStart.getValue(),fChartSlut.getValue());
         List<DataCount> laserData = dc.importData("Digital fabrikation med laserskærer",fChartStart.getValue(),fChartSlut.getValue());
         List<DataCount> robotPåJobData = dc.importData("Robot på job",fChartStart.getValue(),fChartSlut.getValue());
@@ -138,6 +148,14 @@ public class StatestikController {
         forløbChart.setData(forløbData);
         forløbChart.setStartAngle(180);
         forløbChart.getLabelsVisible();
+
+        final Label pieLabel = new Label("");
+        pieLabel.setTextFill(Color.BLACK);
+        pieLabel.setStyle("-fx-font: 20 ARIAL;");
+
+        createToolTips(forløbChart);
+
+        forløbData.forEach(data -> data.nameProperty().bind(Bindings.concat(data.getName())));
     }
 
     @FXML
@@ -190,7 +208,8 @@ public class StatestikController {
                 new PieChart.Data("Robotten rydder op",ro),
                 new PieChart.Data("Naturturisme ved Vadehavet",natur),
                 new PieChart.Data("Skab sikkerhed i Vadehavet",sik),
-                new PieChart.Data("Ingen",ing));
+                new PieChart.Data("Ingen",ing)
+                );
 
         forløbChart.setLegendVisible(true);
         forløbChart.setLabelsVisible(true);
@@ -198,6 +217,15 @@ public class StatestikController {
         forløbChart.setData(forløbData);
         forløbChart.setStartAngle(180);
         forløbChart.getLabelsVisible();
+
+        final Label pieLabel = new Label("");
+        pieLabel.setTextFill(Color.BLACK);
+        pieLabel.setStyle("-fx-font: 20 ARIAL;");
+
+
+        createToolTips(forløbChart);
+
+        forløbData.forEach(data -> data.nameProperty().bind(Bindings.concat(data.getName())));
     }
 
     @FXML
@@ -342,6 +370,24 @@ public class StatestikController {
         fChartStart.setValue(LocalDate.now());
         orgChart.setAnimated(false);
         forløbChart.setAnimated(false);
+    }
+
+    private void createToolTips(PieChart pc) {
+
+        for (PieChart.Data data: pc.getData()) {
+            String msg = data.getName() + ": " + data.getPieValue();
+
+            Tooltip tp = new Tooltip(msg);
+            tp.setShowDelay(Duration.seconds(0));
+
+            Tooltip.install(data.getNode(), tp);
+
+            //update tooltip data when changed
+            data.pieValueProperty().addListener((observable, oldValue, newValue) ->
+            {
+                tp.setText(newValue.toString());
+            });
+        }
     }
 
 
